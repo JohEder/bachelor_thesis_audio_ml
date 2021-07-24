657ff46aa01d4c01e2a587b0165e2d01480c0202
Johannes Eder <johannes@turinger.mobile.ifi.lmu.de>
2021-07-24 18:44:25 +0200

    adjusted balancing of trainig/validation/test split

--- training_setup.py
+++ training_setup.py
@@ -1,5 +1,7 @@
 import datetime
 import math
+import models.autoencoder
+from torch import nn
 
 from sklearn.utils import shuffle
 import models.transformer
@@ -15,6 +17,7 @@ from sklearn.metrics import roc_auc_score, roc_curve
 import logging
 from utils import load_model, plot_and_save_loss_curve, save_model, plot_and_save_roc_curve, save_hyperparams
 from transformers.optimization import get_cosine_schedule_with_warmup, get_linear_schedule_with_warmup
+from transformers import AdamW
 import pytorch_model_summary as pms
 import copy
 
@@ -30,36 +33,34 @@ class TrainingSetup():
     def __str__(self):
         return f'Normal classes: {self.normal_data} Anomalous:{self.anomalous_data}'
 
-    def run(self, number_of_runs):
+    def run(self, number_of_runs=1):
       auc_roc_scores = []
       for i in range(number_of_runs):
         model_name = self.setup_name
-        print(f"\nrunning {i}. Run of Setup: {self.normal_data}")
-        normal_train_data, val_data, test_data = self.get_normal_and_anomalous_data( all_annotations_txt, BATCH_SIZE, BATCH_SIZE_VAL)
-        train_loader = torch.utils.data.DataLoader(normal_train_data, batch_size=BATCH_SIZE, shuffle=True)
-        val_loader = torch.utils.data.DataLoader(val_data, batch_size=BATCH_SIZE_VAL, shuffle=True)
-        test_loader = torch.utils.data.DataLoader(test_data, batch_size=BATCH_SIZE_VAL, shuffle=False)
+        print(f"\nrunning {i + 1}. Run of Setup: {self.normal_data}")
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         if self.model_type == MODEL_TYPES.TRANSFORMER:
+          train_loader, val_loader, test_loader = self.get_normal_and_anomalous_data( all_annotations_txt, BATCH_SIZE, BATCH_SIZE_VAL, MODEL_TYPES.TRANSFORMER)
           #training
           transformer = TransformerModel(EMBEDDING_SIZE, input_dim, N_HEADS, DIM_FEED_FORWARD, N_ENCODER_LAYERS)
           LEARNING_RATE = 0.00001
-          WEIGHT_DECAY = 0.001
-          optimizer = torch.optim.Adam(transformer.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY) 
-          EPOCHS = 1 #later over hundred
+          WEIGHT_DECAY = 0.0001
+          #optimizer = torch.optim.Adam(transformer.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY) 
+          optimizer = AdamW(transformer.parameters(), lr=LEARNING_RATE)
+          EPOCHS = 5 #later over hundred
           total_steps = len(train_loader) * EPOCHS
           warm_up_steps = math.ceil(total_steps * 0.1)
           scheduler = get_linear_schedule_with_warmup(optimizer, warm_up_steps, total_steps)
           print(f"Total Steps: {total_steps}, Warm up steps: {warm_up_steps}, Ratio: {warm_up_steps / total_steps}")
           transformer.to(device)
           transformer.train() #mode
-          roc_auc_best = 0
+          roc_auc_best = -1
           best_model = None
           training_start = datetime.datetime.now()
           losses = []
           for epoch in range(1, EPOCHS + 1):
-            losses_epoch = models.transformer.train_epoch(transformer, train_loader, optimizer, scheduler, epoch, device)
-            val_anom_scores, val_targets = models.transformer.evaluate(transformer, val_loader, device) #batch size in evalution is only one
+            losses_epoch = models.transformer.train_epoch(transformer, train_loader, optimizer, epoch, device, scheduler=scheduler)
+            val_anom_scores, val_targets = models.transformer.get_anom_scores(transformer, val_loader, device) #batch size in evalution is only one
             roc_auc = roc_auc_score(val_targets, val_anom_scores)
             losses += losses_epoch
             if roc_auc > roc_auc_best:
@@ -70,48 +71,84 @@ class TrainingSetup():
             print(f"Evaluation ROC Score in epoch {epoch} is {roc_auc}, Best ROC Score is:{roc_auc_best}")
           training_finished = datetime.datetime.now()
           total_training_time = training_finished - training_start
+          summary = pms.summary(transformer, torch.ones(BATCH_SIZE, 22, 256).to(device))
         elif self.model_type == MODEL_TYPES.AUTOENCODER:
-          print("autoencoder")
-          raise Exception("Not implemented yet")
+          train_loader, val_loader, test_loader = self.get_normal_and_anomalous_data( all_annotations_txt, BATCH_SIZE, BATCH_SIZE_VAL, MODEL_TYPES.AUTOENCODER)
+          #print(next(iter(train_loader)).shape)
+          LEARNING_RATE = 0.001
+          EPOCHS = 1
+          autoencoder = models.autoencoder.AutoEncoder(input_dim=512)
+          optimizer = torch.optim.Adam(autoencoder.parameters(), lr=LEARNING_RATE)
+
+          autoencoder.to(device)
+          autoencoder.train() #mode
+          roc_auc_best = -1
+          best_model = None
+          training_start = datetime.datetime.now()
+          losses = []
+          for epoch in range(1, EPOCHS + 1):
+            losses_epoch = models.autoencoder.train_epoch(autoencoder, train_loader, optimizer, epoch, device)
+            val_anom_scores, val_targets = models.autoencoder.get_anom_scores(autoencoder, val_loader, device) #batch size in evalution is only one
+            roc_auc = roc_auc_score(val_targets, val_anom_scores)
+            losses += losses_epoch
+            if roc_auc > roc_auc_best:
+              best_model = copy.deepcopy(autoencoder)
+              #model_name = save_model(model_name, transformer, epoch)
+              roc_auc_best = roc_auc
+              #print(f"saved model with best validaton in epoch{epoch}")
+            print(f"Evaluation ROC Score in epoch {epoch} is {roc_auc}, Best ROC Score is:{roc_auc_best}")
+          training_finished = datetime.datetime.now()
+          total_training_time = training_finished - training_start
+          summary = pms.summary(autoencoder, torch.ones(BATCH_SIZE, 512).to(device))
+
         elif self.model_type == MODEL_TYPES.IDNN:
           raise Exception("not implemented")
         #evaluation
         roc_auc = self.evaluate_model(best_model, test_loader, device)
         print(f"ROC AUC of Model {model_name} is {roc_auc}!")
         auc_roc_scores.append(roc_auc)
-        summary = pms.summary(transformer, torch.ones(16, 22, 256).to(device))
         plot_and_save_loss_curve(self.setup_name, losses)
-        save_hyperparams(self.model_type,model_name, total_training_time, optimizer, LEARNING_RATE, EPOCHS, self.normal_data, self.anomalous_data, roc_auc, summary, weight_decay=WEIGHT_DECAY, total_steps=total_steps, warm_up_steps=warm_up_steps)
+        save_hyperparams(self.model_type,model_name, total_training_time, optimizer, LEARNING_RATE, EPOCHS, self.normal_data, self.anomalous_data, roc_auc, summary, weight_decay="", total_steps="", warm_up_steps="")
       return auc_roc_scores
 
 
     def evaluate_model(self, best_model, test_loader,device):
       #model = load_model(model_name)
       if self.model_type == MODEL_TYPES.TRANSFORMER:
-        test_anom_scores, test_targets = models.transformer.evaluate(best_model, test_loader, device)
+        test_anom_scores, test_targets = models.transformer.get_anom_scores(best_model, test_loader, device)
         fp_rate, tp_rate, _ = roc_curve(test_targets, test_anom_scores, pos_label=1)
         roc_auc = roc_auc_score(test_targets, test_anom_scores)
         plot_and_save_roc_curve(self.setup_name, fp_rate, tp_rate, roc_auc)
         return roc_auc
-      else:
-        raise Exception("Not implemented")
+      elif self.model_type == MODEL_TYPES.AUTOENCODER:
+        test_anom_scores, test_targets = models.autoencoder.get_anom_scores(best_model, test_loader, device)
+        fp_rate, tp_rate, _ = roc_curve(test_targets, test_anom_scores, pos_label=1)
+        roc_auc = roc_auc_score(test_targets, test_anom_scores)
+        plot_and_save_roc_curve(self.setup_name, fp_rate, tp_rate, roc_auc)
+        return roc_auc
+      elif self.model_type == MODEL_TYPES.IDNN:
+        raise Exception("Not implemented!")
 
-    def get_normal_and_anomalous_data(self, annotations, batch_size, batch_size_test):
+
+    def get_normal_and_anomalous_data(self, annotations, batch_size, batch_size_test, model_type):
         if len((set(self.normal_data) & set(self.anomalous_data))) > 0:
           raise Exception("Intersection between normal and anomalous classes should be empty!")
 
         all_data = import_idmt_traffic_dataset(annotations)
 
         normal_data = all_data[all_data.vehicle.isin(self.normal_data)]
-        normal_data = self.balance_data_by_vehicle(normal_data)
+        #normal_data = self.balance_data_by_vehicle(normal_data)
 
         anomalous_data = all_data[all_data.vehicle.isin(self.anomalous_data)]
-        anomalous_data = self.balance_data_by_vehicle(anomalous_data)
+        #anomalous_data = self.balance_data_by_vehicle(anomalous_data)
 
         train_data, test_data_normal = train_test_split(normal_data, test_size=0.1, shuffle=True, random_state=config.RANDOM_SEED, stratify=normal_data.vehicle)
         train_data, val_data_normal = train_test_split(train_data, test_size=0.01,shuffle=True, random_state=config.RANDOM_SEED, stratify=train_data.vehicle)
         train_data = self.adjust_sample_number_to_batch_size(train_data, batch_size)
         #print(f"training with {len(train_data)} (normal) samples")
+
+        test_data_normal = self.balance_data_by_vehicle(test_data_normal) #balancing test and val data by class to avoid wrong conclusions
+        val_data_normal = self.balance_data_by_vehicle(val_data_normal)
 
         number_of_normal_test_samples_per_categroy= len(test_data_normal) // len(self.normal_data)
         #print(f"testing with {number_of_normal_test_samples_per_categroy} normal samples")
@@ -120,9 +157,9 @@ class TrainingSetup():
         #sample same number of anomalous data to test
         #number_anomlous = number_of_normal_test_sampels if number_of_normal_test_sampels < len(anomalous_data) else len(anomalous_data)
 
-        anomalous_test_data = anomalous_data.sample(number_of_normal_test_samples_per_categroy*len(self.anomalous_data), replace=True, random_state=config.RANDOM_SEED)
-        anomalous_val_data = anomalous_test_data[0:len(val_data_normal)]
-        anomalous_test_data = anomalous_test_data[len(val_data_normal) + 1:] #take first samples from anomalous test data for validation druing trianing
+        anomalous_test_data = anomalous_data.sample(len(test_data_normal), replace=True, random_state=config.RANDOM_SEED)
+        anomalous_val_data = anomalous_data.sample(len(val_data_normal), replace=True, random_state=config.RANDOM_SEED)
+        #anomalous_test_data = anomalous_test_data[len(val_data_normal) + 1:] #take first samples from anomalous test data for validation druing trianing
         #print(f"testing with {len(anomalous_data)} anomalous samples")
         #print(f"Validating with {len(anomalous_val_data)} anomalous samples")
 
@@ -139,11 +176,15 @@ class TrainingSetup():
         print(f"Train Data: \n{train_data['vehicle'].value_counts()} \n\nDistribution in Train data: \n{train_data['vehicle'].value_counts(normalize=True)}")
         print(f"Test Data: \n{concatenated_test_data['vehicle'].value_counts()}\n\nDistribution in Test data: \n{concatenated_test_data['vehicle'].value_counts(normalize=True)}")
         print(f"Validation Data: \n{concatenated_val_data['vehicle'].value_counts()} \n\nDistribution in Validation data: \n{concatenated_val_data['vehicle'].value_counts(normalize=True)}")
-        normal_train_data = IdmtTrafficDataSet(train_data, SAMPLE_RATE, self.normal_data, MODEL_TYPES.TRANSFORMER,on_the_fly=True)
-        val_data = IdmtTrafficDataSet(concatenated_val_data, SAMPLE_RATE, self.normal_data, MODEL_TYPES.TRANSFORMER, on_the_fly=True)
-        test_data = IdmtTrafficDataSet(concatenated_test_data, SAMPLE_RATE, self.normal_data, MODEL_TYPES.TRANSFORMER, on_the_fly=True)
+        normal_train_data = IdmtTrafficDataSet(train_data, SAMPLE_RATE, self.normal_data, model_type=model_type,on_the_fly=True)
+        val_data = IdmtTrafficDataSet(concatenated_val_data, SAMPLE_RATE, self.normal_data, model_type=model_type, on_the_fly=True)
+        test_data = IdmtTrafficDataSet(concatenated_test_data, SAMPLE_RATE, self.normal_data, model_type=model_type, on_the_fly=True)
 
-        return normal_train_data, val_data, test_data
+        train_loader = torch.utils.data.DataLoader(normal_train_data, batch_size=BATCH_SIZE, shuffle=True)
+        val_loader = torch.utils.data.DataLoader(val_data, batch_size=BATCH_SIZE_VAL, shuffle=True)
+        test_loader = torch.utils.data.DataLoader(test_data, batch_size=BATCH_SIZE_VAL, shuffle=False)
+
+        return train_loader, val_loader, test_loader
 
 
     def adjust_sample_number_to_batch_size(self, data, batch_size):
@@ -160,6 +201,9 @@ class TrainingSetup():
       g = pd_data_frame.groupby('vehicle')
       g = pd.DataFrame(g.apply(lambda x: x.sample(g.size().min()).reset_index(drop=True)))
       return g
+    
+    #def balance_data_normal_anomalous(self, normal_data):
+
 
 #Klassse ist verantwortlich die Daten zu holen, das model zu trainieren
 #output: trainiertes model und loss curve
